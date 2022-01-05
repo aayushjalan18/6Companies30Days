7c7e842c4c26db1bea402e11064f396dcf1e0f29
aayushjalan18 <96924009+aayushjalan18@users.noreply.github.com>
2022-01-05 20:26:13 +0530

    Create Question_014.cpp
    Sample Input : 
    6
    2 3 1 2 4 3
    7
    
    Sample Output : 
    2

---
+++ Question_014.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int target, vector<int>& nums) {
+	
+	int n = (int)nums.size();
+    int j = 0, s = 0, ans = n+1;
+    
+    for(int i=0; i<n; i++) {
+        s += nums[i];
+        while(s >= target) {
+            ans = min(ans, i-j+1);
+            s -= nums[j++];
+        }
+    }
+    
+    return ans == n+1 ? 0 : ans;
+	
+}
+
+int main() {
+	
+	int n;
+	cin >> n;
+	
+	vector<int> a(n);
+	for(int i=0; i<n; i++)
+		cin >> a[i];
+		
+	int target;
+	cin >> target;
+	
+	cout << solve(target, a) << '\n';
+	
+	return 0;
+}

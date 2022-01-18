d25d2b27114732a7cdcda5cac914ed3ee0d92b63
aayushjalan18 <96924009+aayushjalan18@users.noreply.github.com>
2022-01-18 18:43:30 +0530

    Create Question_06.cpp
    Sample Input : 
    9 3
    1 2 3 1 4 5 2 3 6
    
    Sample Output : 
    3 3 4 5 5 5 6

---
+++ Amazon/Question_06.cpp
@@ -0,0 +1,39 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solve(int n, int k, vector<int>& arr) {
+	
+    arr.insert(arr.begin(), 0);
+        
+    multiset<int> ms;
+    for(int i=0; i<k; i++)
+        ms.insert(arr[i]);
+        
+    vector<int> ans;
+    for(int i=k; i<=n; i++) {
+        ms.insert(arr[i]);
+        ms.erase(ms.find(arr[i-k]));
+        auto it = ms.end();
+        ans.push_back(*(--it));
+    }
+    
+    return ans;
+    
+}
+
+int main() {
+	
+	int n, k;
+	cin >> n >> k;
+	
+	vector<int> a(n);
+	for(int i=0; i<n; i++)
+		cin >> a[i];
+		
+	vector<int> res = solve(n, k, a);
+	for(auto r : res)
+		cout << r << ' ';
+	
+	return 0;
+}

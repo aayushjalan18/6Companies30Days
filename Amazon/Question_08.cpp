2a14f560ac11c16a2cccfc3e36e28da2572009ec
aayushjalan18 <96924009+aayushjalan18@users.noreply.github.com>
2022-01-18 18:44:55 +0530

    Create Question_08.cpp
    Sample Input : 
    5
    
    Sample Output : 
    3

---
+++ Amazon/Question_08.cpp
@@ -0,0 +1,18 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int solve(int m) {
+	
+	return m/2 + 1;
+}
+
+int main() {
+	
+	int n;
+	cin >> n;
+	
+	cout << solve(n) << '\n';
+	
+	return 0;
+}

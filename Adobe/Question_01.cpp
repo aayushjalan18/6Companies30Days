899c0109e2773133d52501b2b0105ea675a0ccca
aayushjalan18 <96924009+aayushjalan18@users.noreply.github.com>
2022-01-24 19:23:45 +0530

    Create Question_01.cpp

---
+++ Adobe/Question_01.cpp
@@ -0,0 +1,37 @@
+
+#include <bits/stdc++.h>
+using namespace std;
+
+vector<int> solve(vector<int>& arr, int n, long long s) {
+	
+	int i = 0, j = 0;
+    long long sum = 0;
+    while(i < n) {
+        sum += arr[i++]*1LL;
+        while(sum > s) {
+            sum -= arr[j++]*1LL;
+        }
+        if(sum == s) {
+            return {j+1,i};
+        }
+    }
+    return {-1};
+    
+}
+
+int main() {
+	
+	int n, s;
+	cin >> n >> s;
+	
+	vector<int> a(n);
+	for(auto& e : a)
+		cin >> e;
+		
+	vector<int> res = solve(a, n, s*1LL);
+	
+	for(auto r : res)
+		cout << r << ' ';
+	
+	return 0;
+}
